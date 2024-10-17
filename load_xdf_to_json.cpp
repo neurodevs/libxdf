9a0bd62658de71d1abbb9980d8b925d04b829406
eric-yates <hello@ericthecurious.com>
2024-10-16 23:02:46 -0600

    patch: create load_xdf_to_json function

---
+++ load_xdf_to_json.cpp
@@ -0,0 +1,56 @@
+#include "xdf.h"
+#include <json.hpp>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using json = nlohmann::json;
+
+extern "C" char* load_xdf_to_json(const char* filename) {
+    Xdf xdf_instance;
+    std::string filePath(filename);
+
+    xdf_instance.load_xdf(filePath);
+
+    json root;
+    root["stream_count"] = static_cast<int>(xdf_instance.streams.size());
+
+    for (size_t i = 0; i < xdf_instance.streams.size(); i++) {
+        const Xdf::Stream& stream = xdf_instance.streams[i];
+        json streamJson;
+        streamJson["stream_id"] = static_cast<int>(i);
+
+        json infoJson;
+        infoJson["stream_name"] = stream.info.name;
+        infoJson["stream_type"] = stream.info.type;
+        infoJson["channel_count"] = stream.info.channel_count;
+        infoJson["nominal_srate"] = stream.info.nominal_srate;
+        infoJson["channel_format"] = stream.info.channel_format;
+
+        streamJson["info"] = infoJson;
+
+        for (size_t j = 0; j < stream.time_series.size(); j++) {
+            json channelJson;
+            for (size_t k = 0; k < stream.time_series[j].size(); k++) {
+                channelJson.push_back(stream.time_series[j][k]);
+            }
+            streamJson["time_series"].push_back(channelJson);
+        }
+
+        for (size_t j = 0; j < stream.time_stamps.size(); j++) {
+            streamJson["time_stamps"].push_back(stream.time_stamps[j]);
+        }
+
+        root["streams"].push_back(streamJson);
+    }
+
+    std::string serialized_data = root.dump();
+    char* cstr = new char[serialized_data.length() + 1];
+    std::strcpy(cstr, serialized_data.c_str());
+
+    return cstr;
+}
+
+extern "C" void free_string(char* str) {
+    delete[] str;
+}
